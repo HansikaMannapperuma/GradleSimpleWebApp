0862aca2fad585229f678943e4533babe7c94324
Hansika Mannapperuma <hansikamannapperuma@gmail.com>
2023-02-17 16:23:07 +0530

    created application.java class

--- simpleWebApp/src/main/java/com/example/simpleWebApp/springboot/HelloController.java
+++ simpleWebApp/src/main/java/com/example/simpleWebApp/springboot/HelloController.java
@@ -1,9 +1,12 @@
 package com.example.simpleWebApp.springboot;
 
+
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+
 public class HelloController {
     @GetMapping("/")
     public String index() {
